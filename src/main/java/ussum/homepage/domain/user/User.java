477a23ee8736b4506fbb94e08062152547342ef0
Jeoongu <ms235280@naver.com>
2024-07-12 19:41:06 +0900

    [fix] : User 도메인 수정

--- src/main/java/ussum/homepage/domain/user/User.java
+++ src/main/java/ussum/homepage/domain/user/User.java
@@ -15,13 +15,23 @@ public class User {
     private Long studentId;
     private String profileImage;
     private Long kakaoId;
+    private String studentStatus; // 학적상
     private String createdAt;
     private String updatedAt;
 
     // 수정 필요
-    public static User of(Long id, String name, Long studentId, String profileImage, Long kakaoId,
+    public static User of(Long id, String name, Long studentId, String profileImage, Long kakaoId, String studentStatus,
                           LocalDateTime createdAt, LocalDateTime updatedAt) {
-        return new User(id, name, studentId, profileImage, kakaoId, String.valueOf(createdAt), String.valueOf(updatedAt));
+        return User.builder().
+                id(id).
+                name(name).
+                studentId(studentId).
+                profileImage(profileImage).
+                kakaoId(kakaoId).
+                studentStatus(studentStatus).
+                createdAt(String.valueOf(createdAt)).
+                updatedAt(String.valueOf(updatedAt)).
+                build();
     }
 
     public static User createUser(KakaoUserInfoResponseDto userInfoResponseDto) {
