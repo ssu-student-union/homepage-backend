78a77dc0beed0e4016aa369ee58dab5c51800403
Jeoongu <ms235280@naver.com>
2024-07-28 23:58:51 +0900

    [feat] : #26 CSVReader

---
+++ src/main/java/ussum/homepage/infra/csvBatch/csv/CSVReader.java
@@ -0,0 +1,45 @@
+package ussum.homepage.infra.csvBatch.csv;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import ussum.homepage.infra.csvBatch.csv.entity.StudentCsvDto;
+
+@Configuration
+@RequiredArgsConstructor
+public class CSVReader {
+    @Bean
+    public FlatFileItemReader<StudentCsvDto> csvFileItemReader() { // 엔티티를 반환하는게 맞나?
+        /* file read */
+        FlatFileItemReader<StudentCsvDto> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(new ClassPathResource("**/src/main/resources/csv/학생목록.csv"));
+        flatFileItemReader.setLinesToSkip(1); // header line skip
+        flatFileItemReader.setEncoding("UTF-8"); // encoding
+        // flatFileItemReader.setRecordSeparatorPolicy(new DefaultRecordSeparatorPolicy());
+        // 개행이 있을 시 사용
+
+        /* read하는 데이터를 내부적으로 LineMapper을 통해 Mapping */
+        DefaultLineMapper<StudentCsvDto> defaultLineMapper = new DefaultLineMapper<>();
+
+        /* delimitedLineTokenizer : setNames를 통해 각각의 데이터의 이름 설정 */
+        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer(",");
+        delimitedLineTokenizer.setNames("id", "name");
+        defaultLineMapper.setLineTokenizer(delimitedLineTokenizer);
+
+        /* beanWrapperFieldSetMapper : Tokenizer에서 가지고온 데이터들을 VO로 바인드하는 역할 */
+        BeanWrapperFieldSetMapper<StudentCsvDto> beanWrapperFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        beanWrapperFieldSetMapper.setTargetType(StudentCsvDto.class);
+
+        defaultLineMapper.setFieldSetMapper(beanWrapperFieldSetMapper);
+
+        /* lineMapper 지정 */
+        flatFileItemReader.setLineMapper(defaultLineMapper);
+
+        return flatFileItemReader;
+    }
+}
