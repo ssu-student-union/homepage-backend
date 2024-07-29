72d10b9faa41b9a1af64dfa0de439acd0f4a4a61
Jeoongu <ms235280@naver.com>
2024-07-29 17:19:30 +0900

    [fix] : #26 S3Config

--- src/main/java/ussum/homepage/infra/config/S3Config.java
+++ src/main/java/ussum/homepage/infra/config/S3Config.java
@@ -12,10 +12,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
     @Value("${cloud.aws.region.static}")
     private String region;
 
@@ -23,8 +25,8 @@ public class S3Config {
     public AmazonS3 s3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
     }
 }
