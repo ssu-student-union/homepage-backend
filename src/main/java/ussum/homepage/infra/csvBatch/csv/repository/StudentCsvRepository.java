3f5245db5defd396e0a0ea6c9f4f3b484b844693
Jeoongu <ms235280@naver.com>
2024-07-31 15:13:39 +0900

    [feat] : #26 StudentCsvRepository

--- src/main/java/ussum/homepage/infra/csvBatch/csv/repository/StudentCsvRepository.java
+++
@@ -1,8 +0,0 @@
-package ussum.homepage.infra.csvBatch.csv.repository;
-
-import org.springframework.batch.item.Chunk;
-import ussum.homepage.infra.csvBatch.csv.entity.StudentCsvData;
-
-public interface StudentCsvRepository {
-    void saveAll(Chunk<StudentCsvData> students);
-}
