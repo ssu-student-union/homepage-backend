f68994dd8ed0eb05f69c2ba6aef956d0ddac6c37
Jeoongu <ms235280@naver.com>
2024-07-31 15:16:44 +0900

    [fix] : #26 JobRunner 수정

--- src/main/java/ussum/homepage/infra/csvBatch/JobLauncherRunner.java
+++ src/main/java/ussum/homepage/infra/csvBatch/JobLauncherRunner.java
@@ -5,22 +5,25 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JobLauncherRunner implements CommandLineRunner {
+public class JobLauncherRunner {
+
     @Autowired
     private JobLauncher jobLauncher;
 
     @Autowired
     private Job studentDataLoadJob;
 
-    @Override
-    public void run(String... args) throws Exception {
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addString("JobID", String.valueOf(System.currentTimeMillis()))
-                .toJobParameters();
-        jobLauncher.run(studentDataLoadJob, jobParameters);
+    public void runJob() {
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addString("JobID", String.valueOf(System.currentTimeMillis()))
+                    .toJobParameters();
+            jobLauncher.run(studentDataLoadJob, jobParameters);
+        } catch (Exception e) {
+            throw new RuntimeException("CSV를 DB에 넣던 중에 에러가 발생했습니다.", e);
+        }
     }
 }
