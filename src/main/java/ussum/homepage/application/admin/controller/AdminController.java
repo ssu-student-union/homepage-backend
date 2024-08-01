8d228fe7ebd89067b0d31cffd5f91318d98f77a3
Jeoongu <ms235280@naver.com>
2024-08-01 23:47:36 +0900

    [fix] : #26 AdminController 반환값 수정

--- src/main/java/ussum/homepage/application/admin/controller/AdminController.java
+++ src/main/java/ussum/homepage/application/admin/controller/AdminController.java
@@ -1,6 +1,7 @@
 package ussum.homepage.application.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,14 +17,14 @@ public class AdminController {
     private final AdminService adminService;
 
     @PostMapping("/s3-csv")
-    public ApiResponse<?> csvToS3(@RequestParam MultipartFile file) {
+    public ResponseEntity<ApiResponse<?>> csvToS3(@RequestParam MultipartFile file) {
         adminService.uploadCsvToS3(file);
-        return ApiResponse.onSuccess(null);
+        return ApiResponse.success(null);
     }
 
     @PostMapping("csv-project")
-    public ApiResponse<?> s3ToProject(@RequestParam("file") String fileName) {
+    public ResponseEntity<ApiResponse<?>> s3ToProject(@RequestParam("file") String fileName) {
         adminService.uploadCsvFromS3ToProject(fileName);
-        return ApiResponse.onSuccess("CSV파일을 DB에 성공적으로 저장했습니다.");
+        return ApiResponse.success(null);
     }
 }
