5a9eab8c6a82e27be0a9f49380730b5156227a97
qogustj <haulqogustj@naver.com>
2024-08-02 11:42:50 +0900

    [chore] switch 사용로직 주석처리

--- src/main/java/ussum/homepage/application/post/service/PostManageService.java
+++ src/main/java/ussum/homepage/application/post/service/PostManageService.java
@@ -62,3 +62,82 @@ public class PostManageService {
         return PostListRes.of(responseList, pageInfo);
     }
 }
+//스위치 사용 로직
+/*
+package ussum.homepage.application.post.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import ussum.homepage.application.post.service.dto.response.postList.*;
+import ussum.homepage.domain.post.Board;
+import ussum.homepage.domain.post.Post;
+import ussum.homepage.domain.post.service.BoardReader;
+import ussum.homepage.domain.post.service.PostReader;
+import ussum.homepage.global.common.PageInfo;
+
+import java.util.List;
+
+import static ussum.homepage.global.error.status.ErrorStatus.POST_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
+public class PostManageService {
+
+    private final BoardReader boardReader;
+    private final PostReader postReader;
+
+    public PostListRes<?> getPostList(int page, int take, String boardCode) {
+        Board board = boardReader.getBoardWithBoardCode(boardCode);
+        Pageable pageable = PageInfo.of(page, take);
+        Page<Post> postList = postReader.getPostListByBoardId(board.getId(), pageable);
+        PageInfo pageInfo = PageInfo.of(postList);
+
+        switch (board.getName()) {
+            case "공지사항":
+                return getNoticePostList(postList, pageInfo);
+            case "분실물":
+                return getLostPostList(postList, pageInfo);
+            case "제휴":
+                return getPartnerPostList(postList, pageInfo);
+            case "감사기구":
+                return getAuditPostList(postList, pageInfo);
+            case "청원":
+                // return getPetitionPostList(postList, pageInfo);
+            default:
+                throw new EntityNotFoundException(String.valueOf(POST_NOT_FOUND));
+        }
+    }
+
+    private PostListRes<NoticePostResponse> getNoticePostList(Page<Post> postList, PageInfo pageInfo) {
+        List<NoticePostResponse> responseList = postList.getContent().stream()
+                .map(NoticePostResponse::of)
+                .toList();
+        return PostListRes.of(responseList, pageInfo);
+    }
+
+    private PostListRes<LostPostResponse> getLostPostList(Page<Post> postList, PageInfo pageInfo) {
+        List<LostPostResponse> responseList = postList.getContent().stream()
+                .map(LostPostResponse::of)
+                .toList();
+        return PostListRes.of(responseList, pageInfo);
+    }
+
+    private PostListRes<PartnerPostResponse> getPartnerPostList(Page<Post> postList, PageInfo pageInfo) {
+        List<PartnerPostResponse> responseList = postList.getContent().stream()
+                .map(PartnerPostResponse::of)
+                .toList();
+        return PostListRes.of(responseList, pageInfo);
+    }
+
+    private PostListRes<AuditPostResponseDto> getAuditPostList(Page<Post> postList, PageInfo pageInfo) {
+        List<AuditPostResponseDto> responseList = postList.getContent().stream()
+                .map(AuditPostResponseDto::of)
+                .toList();
+        return PostListRes.of(responseList, pageInfo);
+    }
+}
+
+ */
