e65cb2802a606afa3ed2940d7d6dc20ec9fd9f60
chahyunsoo <chahs1012@naver.com>
2024-08-13 23:33:17 +0900

    [refactor] 댓글 생성 시 OFFICIAL 댓글 생성 로직 수정

--- src/main/java/ussum/homepage/domain/member/MemberRepository.java
+++ src/main/java/ussum/homepage/domain/member/MemberRepository.java
@@ -4,4 +4,6 @@ import java.util.Optional;
 
 public interface MemberRepository {
     Optional<Member> findByUserId(Long userId);
+    Optional<Member> findCentralOperationCommitteeMember(Long userId);
+
 }
