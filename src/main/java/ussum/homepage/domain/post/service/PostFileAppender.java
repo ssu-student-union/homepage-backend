d52c793641f77ba3c2f1cb568f881049d0462e38
Jeoongu <ms235280@naver.com>
2024-08-14 11:18:07 +0900

    [feat] : #48 PostFile save, findById, saveAll 메소드 추가

--- src/main/java/ussum/homepage/domain/post/service/PostFileAppender.java
+++ src/main/java/ussum/homepage/domain/post/service/PostFileAppender.java
@@ -8,6 +8,7 @@ import ussum.homepage.domain.post.PostFile;
 import ussum.homepage.domain.post.PostFileRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +18,23 @@ public class PostFileAppender {
     @Transactional
     public List<PostFile> saveAllPostFile(List<PostFile> fileList) {
         return postFileRepository.saveAll(fileList);
+    }
+
+    @Transactional
+    public void save(List<Long> postFileIds, Long postId){
+        postFileIds.stream()
+                .map(postFileRepository::findById)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .forEach(postFile -> {
+                    PostFile updatedPostFile = PostFile.of(
+                            postFile.getId(),
+                            postFile.getTypeName(),
+                            postFile.getUrl(),
+                            postFile.getSize(),
+                            postId
+                    );
+                    postFileRepository.save(updatedPostFile);
+                });
     }
 }
