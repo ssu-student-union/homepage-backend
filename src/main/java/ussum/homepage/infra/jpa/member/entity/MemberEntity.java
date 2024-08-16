b602d3ac5c53376c0dd6a39715e60106f15ce78f
Jeoongu <ms235280@naver.com>
2024-08-17 03:47:42 +0900

    [feat] : #54 GroupEntity값 null을 고려한 nullGroup 메소드 생성 후 주석 처리

--- src/main/java/ussum/homepage/infra/jpa/member/entity/MemberEntity.java
+++ src/main/java/ussum/homepage/infra/jpa/member/entity/MemberEntity.java
@@ -1,10 +1,10 @@
 package ussum.homepage.infra.jpa.member.entity;
+
 import jakarta.persistence.*;
 import lombok.*;
 import ussum.homepage.infra.jpa.BaseEntity;
 import ussum.homepage.infra.jpa.group.entity.GroupEntity;
 import ussum.homepage.infra.jpa.user.entity.UserEntity;
-
 
 @Entity
 @Table(name = "member")
@@ -28,15 +28,18 @@ public class MemberEntity extends BaseEntity {
     @JoinColumn(name = "user_id")
     private UserEntity userEntity;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST) // 일단  cascade = CascadeType.PERSIST 추가, 영속화 문제 해결
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "groups_id")
     private GroupEntity groupEntity;
 
     public static MemberEntity of(Long id, Boolean isAdmin, MemberCode memberCode, MajorCode majorCode, UserEntity userEntity, GroupEntity groupEntity) {
         return new MemberEntity(id, isAdmin, memberCode, majorCode, userEntity, groupEntity);
-    } // ?
+    }
+    public static MemberEntity nullGroup(Long id, Boolean isAdmin, MemberCode memberCode, MajorCode majorCode, UserEntity userEntity) {
+        return new MemberEntity(id, isAdmin, memberCode, majorCode, userEntity, null);
+    }
 
     public static MemberEntity from(Long id) {
         return new MemberEntity(id, null, null, null, null, null);
-    } // ?
+    }
 }
