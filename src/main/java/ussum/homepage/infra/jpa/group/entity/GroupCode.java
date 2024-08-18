62f87f404bf374f05abd10d6b537ec4531a9485d
chahyunsoo <chahs1012@naver.com>
2024-08-19 03:05:45 +0900

    [refactor] Code Enum 값 한글로 변경

--- src/main/java/ussum/homepage/infra/jpa/group/entity/GroupCode.java
+++ src/main/java/ussum/homepage/infra/jpa/group/entity/GroupCode.java
@@ -12,67 +12,67 @@ import static ussum.homepage.global.error.status.ErrorStatus.INVALID_MAJORCODE;
 @Getter
 public enum GroupCode {
     //관리자
-    ADMIN("ADMIN"),
+    ADMIN("관리자"),
 
     //총학생회
-    STUDENT_UNION("STUDENT_UNION"),
+    STUDENT_UNION("총학생회"),
 
     //중앙기구
-    CENTRAL_ORGANIZATION("CENTRAL_ORGANIZATION"),
+    CENTRAL_ORGANIZATION("중앙기구"),
 
     //선거관리위원회
-    VOTE_COMMITTEE("VOTE_COMMITTEE"),
+    VOTE_COMMITTEE("선거관리위원회"),
 
     //감사위원회
-    AUDIT_COMMITTEE("AUDIT_COMMITTEE"),
+    AUDIT_COMMITTEE("감사위원회"),
 
     //단과대학생회
-    COLLEGE_UNION("COLLEGE_UNION"),
+    COLLEGE_UNION("단과대학생회"),
 
     //학과부학생회
-    DEPARTMENT_UNION("DEPARTMENT_UNION"),
+    DEPARTMENT_UNION("학과부학생회"),
 
     //특별기구
-    SPECIAL_ORGANIZATION("SPECIAL_ORGANIZATION"),
+    SPECIAL_ORGANIZATION("특별기구"),
 
     //인권신고접수
-    STUDENT_RIGHTS_REPORT_SUBMISSION("STUDENT_RIGHTS_REPORT_SUBMISSION"),
+    STUDENT_RIGHTS_REPORT_SUBMISSION("인권신고접수"),
 
     //학생서비스팀
-    STUDENT_SERVICE_TEAM("STUDENT_SERVICE_TEAM"),
+    STUDENT_SERVICE_TEAM("학생서비스팀"),
 
     //학생자치기구
-    STUDENT_GOVERNMENT_ORGANIZATION("STUDENT_GOVERNMENT_ORGANIZATION"),
+    STUDENT_GOVERNMENT_ORGANIZATION("학생자치기구"),
 
     //경영대학
-    BUSINESS_SCHOOL("BUSINESS_SCHOOL"),
+    BUSINESS_SCHOOL("경영대학"),
 
     //경제통상대학
-    ECONOMICS_TRADE_SCHOOL("ECONOMICS_TRADE_SCHOOL"),
+    ECONOMICS_TRADE_SCHOOL("경제통상대학"),
 
     //공과대학
-    ENGINEERING_SCHOOL("ENGINEERING_SCHOOL"),
+    ENGINEERING_SCHOOL("공과대학"),
 
     //법과대학
-    LAW_SCHOOL("LAW_SCHOOL"),
+    LAW_SCHOOL("법과대학"),
 
     //사회과학대학
-    SOCIAL_SCIENCES_SCHOOL("SOCIAL_SCIENCES_SCHOOL"),
+    SOCIAL_SCIENCES_SCHOOL("사회과학대학"),
 
     //인문대학
-    HUMANITIES_SCHOOL("HUMANITIES_SCHOOL"),
+    HUMANITIES_SCHOOL("인문대학"),
 
     //자연과학대학
-    NATURAL_SCIENCES_SCHOOL("NATURAL_SCIENCES_SCHOOL"),
+    NATURAL_SCIENCES_SCHOOL("자연과학대학"),
 
     //IT대학
-    IT_SCHOOL("IT_SCHOOL"),
+    IT_SCHOOL("IT대학"),
 
     //융합특성화자유전공학부
-    CONVERGENCE_DEPARTMENT("INTERDISCIPLINARY_STUDIES_DEPARTMENT"),
+    CONVERGENCE_DEPARTMENT("융합특성화자유전공학부"),
 
     //동아리연합회
-    CLUB_UNION("CLUB_UNION");
+    CLUB_UNION("동아리연합회");
 
     private final String stringGroupCode;
 
