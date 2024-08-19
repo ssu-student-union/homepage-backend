aa14f0b3f057822a972df885103c73eb03ea9e43
chahyunsoo <chahs1012@naver.com>
2024-08-20 01:43:21 +0900

    [feat] swagger description 추가

--- src/main/java/ussum/homepage/application/comment/controller/PostCommentManagerController.java
+++ src/main/java/ussum/homepage/application/comment/controller/PostCommentManagerController.java
@@ -10,7 +10,7 @@ import ussum.homepage.application.post.service.dto.request.PostUserRequest;
 import ussum.homepage.global.ApiResponse;
 
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/board")
 @RestController
 @Tag(name = "post_comment_and_post_reply_comment", description = "게시물 단건 조회 시 댓글과 대댓글을 모두 조회하는 api")
 public class PostCommentManagerController {
@@ -18,15 +18,16 @@ public class PostCommentManagerController {
 
     @Operation(summary = "게시물 단건 조회 시 댓글과 대댓글을 모두 조회하는 api", description = """
             게시물 단건 조회 시 댓글과 대댓글을 모두 조회하는 api 입니다. 
-            
-            요청으로 postId, type(인기순 or 최신순), 그리고 form-data 형식으로 userId의 값으로 Long 형으로 받습니다.
-            
+            요청으로 postId, type(인기순 or 최신순), 그리고 form-data 형식으로 key : userId , value: Long 형의 실제 userId 값을 넣으면 됩니다.
+            userId를 받는 이유는 비로그인과 로그인 모두 조회는 할 수 있고, 
+            이때 로그인 유저가 조회 시 토큰 발급 시 같이 반환 되었던 userId 값을 이용하여, 해당 user가 해당 댓글 혹은 대댓글을 작성하였는지의 여부를 판단하기 위하여 isAuthor 필드를 이용하였습니다.
+            userId를 넣고 반환되는 isAuthor 필드가 true 라면 해당 user는 본인이 작성한 댓글, 대댓글 임을 나타냅니다.
+            하지만 isAuthor 필드가 false 라면(비로그인 포함) 해당 user는 본인이 작성한 댓글, 대댓글이 아니기에 수정, 삭제를 막아야 합니다. 
             """)
-    @GetMapping("/board/posts/{postId}/comments")
+    @GetMapping("/posts/{postId}/comments")
     public ResponseEntity<ApiResponse<?>> getPostCommentList(@PathVariable Long postId,
                                                              @RequestParam(name = "type") String type,
                                                              @ModelAttribute PostUserRequest postUserRequest) { //type은 "인기순" 인지 "최신순"인지
-
         return ApiResponse.success(postCommentManageService.getCommentList(postId, type, postUserRequest));
     }
 }
--- src/main/java/ussum/homepage/application/reaction/controller/PostReplyCommentReactionController.java
+++ src/main/java/ussum/homepage/application/reaction/controller/PostReplyCommentReactionController.java
@@ -6,13 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ussum.homepage.application.reaction.service.PostReplyCommentReactionService;
-import ussum.homepage.application.reaction.service.dto.request.CreatePostCommentReactionReq;
 import ussum.homepage.application.reaction.service.dto.request.CreatePostReplyCommentReactionReq;
 import ussum.homepage.global.ApiResponse;
 import ussum.homepage.global.config.auth.UserId;
 
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/toggle")
 @RestController
 @Tag(name = "post_reply_comment_reaction", description = "게시물 대댓글 반응 api")
 public class PostReplyCommentReactionController {
@@ -20,13 +19,11 @@ public class PostReplyCommentReactionController {
 
     @Operation(summary = "게시물 대댓글 반응 통합 api", description = """
             게시물 대댓글 반응을 위한 통합 api입니다.
-            
+            요청 url 상에 reply-commentId를 받습니다. 즉, 대댓글 id를 받습니다.
             요청 json으로 like 또는 unlike를 받습니다.
-            
             ex. like를 Request에 넣어서 요청을 하면 대댓글 좋아요가 생성되고, 동일한 요청을 한번더 요청하면 이전에 눌렀던 대댓긇 좋아요가 취소됩니다.
-            
             """)
-    @PostMapping("/toggle/posts/comments/{reply-commentId}")
+    @PostMapping("/posts/comments/reply-comments/{reply-commentId}")
     public ResponseEntity<ApiResponse<?>> togglePostReplyCommentReaction(@UserId Long userId,
                                                                     @PathVariable(name = "reply-commentId") Long replyCommentId,
                                                                     @RequestBody CreatePostReplyCommentReactionReq createPostReplyCommentReactionReq) {
