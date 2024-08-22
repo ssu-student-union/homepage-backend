d54a89dc325ba00a95ec7d7ebbae8713730fac9e
Bae Hyeonseo <haulqogustj@naver.com>
2024-08-22 21:38:40 +0900

    [chore] #50 스웨거 수정

--- src/main/java/ussum/homepage/application/post/controller/PostManageController.java
+++ src/main/java/ussum/homepage/application/post/controller/PostManageController.java
@@ -21,8 +21,9 @@ public class PostManageController {
     private final PostManageService postManageService;
     @Operation(summary = "게시판 별 게시물 리스트 조회 api", description = """
             게시판 별 게시물 리스트 조회 시 필요한 데이터를 조회하는 api 입니다.
-            요청으로 boardCode 그리고 queryParam 형식으로 , groupCode(중앙기구, 단과대학생회), memberCode(중앙운영위원회), page(입력 안 할시 첫번째 페이지), take(몇개 가져올지) 값을 넣으면 됩니다.
+            요청으로 boardCode 그리고 queryParam 형식으로 , groupCode(중앙기구, 단과대학생회), memberCode(중앙운영위원회), category(필터링),page(입력 안 할시 첫번째 페이지), take(몇개 가져올지) 값을 넣으면 됩니다.
             공지사항게시판을 사용할때만 groupCode, memberCode에 값을 넣어서 사용하시면 됩니다. 
+            나머지 게시판 필터링은 category에 값을 넣고 사용하시면 됩니다.
             """)
     @GetMapping("/{boardCode}/posts")
     public ResponseEntity<ApiResponse<?>> getBoardPostsList(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "take") int take,
