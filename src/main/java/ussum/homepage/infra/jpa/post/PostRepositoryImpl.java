ead67fe6775e3cbc4ced923a445c1b2e72502044
chahyunsoo <chahs1012@naver.com>
2024-08-27 00:21:32 +0900

    [fix] 게시물 전체 조회시 totalElements 중복 카운트 문제 원상 복귀

--- src/main/java/ussum/homepage/infra/jpa/post/PostRepositoryImpl.java
+++ src/main/java/ussum/homepage/infra/jpa/post/PostRepositoryImpl.java
@@ -228,19 +228,19 @@ public class PostRepositoryImpl implements PostRepository {
                 .limit(pageable.getPageSize())
                 .fetch();
 
-//        JPAQuery<Long> countQuery = queryFactory
-//                .select(postEntity.count())
-//                .from(postEntity)
-//                .leftJoin(postEntity.userEntity, userEntity)
-//                .leftJoin(memberEntity).on(memberEntity.userEntity.eq(userEntity))
-//                .leftJoin(memberEntity.groupEntity, groupEntity)
-//                .leftJoin(postFileEntity).on(postFileEntity.postEntity.eq(postEntity))
-//                .where(whereClause);
-
         JPAQuery<Long> countQuery = queryFactory
                 .select(postEntity.count())
                 .from(postEntity)
+                .leftJoin(postEntity.userEntity, userEntity)
+                .leftJoin(memberEntity).on(memberEntity.userEntity.eq(userEntity))
+                .leftJoin(memberEntity.groupEntity, groupEntity)
+                .leftJoin(postFileEntity).on(postFileEntity.postEntity.eq(postEntity))
                 .where(whereClause);
+
+//        JPAQuery<Long> countQuery = queryFactory
+//                .select(postEntity.count())
+//                .from(postEntity)
+//                .where(whereClause);
 
         return PageableExecutionUtils.getPage(
                 content.stream().map(postMapper::toDomain).collect(Collectors.toList()),
