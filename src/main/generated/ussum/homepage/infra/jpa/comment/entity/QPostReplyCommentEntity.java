7d45245fdbf3d17284b83c309e2ed1ec803fad02
Jeoongu <ms235280@naver.com>
2024-08-27 21:30:36 +0900

    [fix] : q객체 수정된 것 커밋

--- src/main/generated/ussum/homepage/infra/jpa/comment/entity/QPostReplyCommentEntity.java
+++ src/main/generated/ussum/homepage/infra/jpa/comment/entity/QPostReplyCommentEntity.java
@@ -29,7 +29,11 @@ public class QPostReplyCommentEntity extends EntityPathBase<PostReplyCommentEnti
     //inherited
     public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
 
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = createDateTime("deletedAt", java.time.LocalDateTime.class);
+
     public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isDeleted = createBoolean("isDeleted");
 
     public final DateTimePath<java.time.LocalDateTime> lastEditedAt = createDateTime("lastEditedAt", java.time.LocalDateTime.class);
 
