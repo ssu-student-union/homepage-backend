c27904e767d56d1615976e9a7c48b09b4be3c61e
Jeoongu <ms235280@naver.com>
2024-08-28 18:44:30 +0900

    [fix] : 자료형 불일치 문제 해결

--- src/main/java/ussum/homepage/domain/csv_user/service/StudentCsvReader.java
+++ src/main/java/ussum/homepage/domain/csv_user/service/StudentCsvReader.java
@@ -7,6 +7,8 @@ import ussum.homepage.domain.csv_user.StudentCsv;
 import ussum.homepage.domain.csv_user.StudentCsvRepository;
 import ussum.homepage.global.error.exception.GeneralException;
 import ussum.homepage.global.error.status.ErrorStatus;
+import ussum.homepage.infra.jpa.member.entity.MajorCode;
+import ussum.homepage.infra.jpa.member.entity.MemberCode;
 
 import java.util.Optional;
 
@@ -25,9 +27,9 @@ public class StudentCsvReader {
 
     private void checkStudentRight(OnBoardingRequest request, StudentCsv studentCsv){
         boolean name = request.getName().equals(studentCsv.getStudentName());
-        boolean studentId = request.getStudentId().equals(studentCsv.getStudentId());
-        boolean groupName = request.getMemberCode().equals(studentCsv.getGroupName());
-        boolean major = request.getMajorCode().equals(studentCsv.getMajor());
+        boolean studentId = request.getStudentId().equals(studentCsv.getStudentId().toString());
+        boolean groupName = request.getMemberCode().equals(MemberCode.getEnumMemberCodeFromStringMemberCode(studentCsv.getGroupName()).getStringMemberCode());
+        boolean major = request.getMajorCode().equals(MajorCode.getEnumMajorCodeFromStringMajorCode(studentCsv.getMajor()).getStringMajorCode());
 
         if(!(name && studentId && groupName && major)){
             throw new GeneralException(ErrorStatus.INVALID_ONBOARDING_REQUEST);
