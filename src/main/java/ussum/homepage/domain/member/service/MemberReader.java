129cc9b492b09de60040d3bef354bc90d9b21e9e
Jeoongu <ms235280@naver.com>
2024-08-30 04:56:37 +0900

    [fix] : Member List 반화느로 자치기구 로그인 수정

--- src/main/java/ussum/homepage/domain/member/service/MemberReader.java
+++ src/main/java/ussum/homepage/domain/member/service/MemberReader.java
@@ -6,6 +6,8 @@ import ussum.homepage.domain.member.Member;
 import ussum.homepage.domain.member.MemberRepository;
 import ussum.homepage.domain.member.exception.MemberNotFoundException;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static ussum.homepage.global.error.status.ErrorStatus.MEMBER_NOT_FOUND;
@@ -17,6 +19,14 @@ public class MemberReader {
 
     public Member getMemberWithUserId(Long userId) {
         return memberRepository.findByUserId(userId).orElseThrow(() -> new MemberNotFoundException(MEMBER_NOT_FOUND));
+    }
+
+    public List<Member> getMembersWithUserId(Long userId) {
+        List<Member> memberList = memberRepository.findAllByUserId(userId);
+        if (memberList.isEmpty()) {
+            throw new MemberNotFoundException(MEMBER_NOT_FOUND);
+        }
+        return memberList;
     }
 
 }
