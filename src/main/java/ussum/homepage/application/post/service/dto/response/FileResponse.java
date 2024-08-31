06034724e832724d00bd67b44377d61ff058d9c5
Bae Hyeonseo <haulqogustj@naver.com>
2024-08-31 16:24:31 +0900

    [fix] 자료집 조회 dto 수정

--- src/main/java/ussum/homepage/application/post/service/dto/response/FileResponse.java
+++ src/main/java/ussum/homepage/application/post/service/dto/response/FileResponse.java
@@ -6,12 +6,14 @@ import ussum.homepage.domain.post.PostFile;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record FileResponse(
+        Long postFileId,
         String fileName,
         String fileUrl,
         String fileType
 ) {
     public static FileResponse of(PostFile postFile){
         return FileResponse.builder()
+                .postFileId(postFile.getId())
                 .fileName(postFile.getFileName())
                 .fileUrl(postFile.getUrl())
                 .fileType(postFile.getTypeName())
