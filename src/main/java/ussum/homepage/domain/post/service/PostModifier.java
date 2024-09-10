92ff05e7a26db34957c85d563d2d0c97d152cff0
chahyunsoo <chahs1012@naver.com>
2024-09-11 02:25:21 +0900

    [hotfix] 공지시항 게시물 null 처리 해결

--- src/main/java/ussum/homepage/domain/post/service/PostModifier.java
+++ src/main/java/ussum/homepage/domain/post/service/PostModifier.java
@@ -2,10 +2,8 @@ package ussum.homepage.domain.post.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import ussum.homepage.application.post.service.dto.request.PostUpdateRequest;
-import ussum.homepage.domain.acl.service.AclModifier;
-import ussum.homepage.domain.acl.service.AclReader;
 import ussum.homepage.domain.post.*;
 import ussum.homepage.infra.jpa.post.entity.Category;
 
@@ -15,16 +13,12 @@ public class PostModifier {
     private final PostRepository postRepository;
     private final PostReader postReader;
     private final BoardReader boardReader;
-    private final AclModifier aclModifier;
-    private final AclReader aclReader;
 
     public Post updatePost(String boardCode,Long postId, PostUpdateRequest postUpdateRequest){
         Board board = boardReader.getBoardWithBoardCode(boardCode);
-//        Post postWithBoardCode = postReader.getPostWithBoardCodeForEditAndDelete(boardCode, postId);
+        Category category = StringUtils.hasText(postUpdateRequest.categoryCode()) ? Category.getEnumCategoryCodeFromStringCategoryCode(postUpdateRequest.categoryCode()) : null;
 
-        return postRepository.save(postUpdateRequest.toDomain(
-                postReader.getPostWithBoardCodeForEditAndDelete(boardCode, postId), board, Category.getEnumCategoryCodeFromStringCategoryCode(postUpdateRequest.categoryCode()))
-        );
+        return postRepository.save(postUpdateRequest.toDomain(postReader.getPostWithBoardCodeForEditAndDelete(boardCode, postId), board, category));
     }
 
     public void deletePost(String boardCode, Long postId) {
