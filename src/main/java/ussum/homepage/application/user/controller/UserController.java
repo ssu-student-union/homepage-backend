3e8c3683c793a9caf50e054a3de6bc05f484d64f
jeongu <ms235280@naver.com>
2024-10-27 15:18:31 +0900

    [add] 토큰 유저 정보 조회 로직

--- src/main/java/ussum/homepage/application/user/controller/UserController.java
+++ src/main/java/ussum/homepage/application/user/controller/UserController.java
@@ -1,18 +1,33 @@
 package ussum.homepage.application.user.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import ussum.homepage.application.user.service.UserService;
+import ussum.homepage.application.user.service.dto.request.TokenRequest;
+import ussum.homepage.application.user.service.dto.response.UserInfoResponse;
 import ussum.homepage.global.ApiResponse;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
+@Tag(name = "users", description = "유저 관련 api")
 public class UserController {
     private final UserService userService;
+
+    @Operation(summary = "PASSU 정보 조회 API", description = """
+            PASSU 정보 조회 API 입니다. Authorization 헤더로 토큰 보내면 됩니다.
+            """)
+    @GetMapping("/user-info")
+    public ApiResponse<UserInfoResponse> getUserInfo(@RequestBody TokenRequest tokenRequest){
+        log.info("##### userId ##### : " + tokenRequest.accessToken());
+        return ApiResponse.onSuccess(userService.getUserInfo(tokenRequest.accessToken()));
+    }
 }
