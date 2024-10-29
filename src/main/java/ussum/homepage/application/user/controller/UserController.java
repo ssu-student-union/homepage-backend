a11dc353b34b0e6f2aff816c31954b2ac5da0f0b
jeongu <ms235280@naver.com>
2024-10-29 13:23:39 +0900

    [fix] Header로 토큰 발급받는 것으로 수정

--- src/main/java/ussum/homepage/application/user/controller/UserController.java
+++ src/main/java/ussum/homepage/application/user/controller/UserController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ussum.homepage.application.user.service.UserService;
@@ -22,11 +23,13 @@ public class UserController {
     private final UserService userService;
 
     @Operation(summary = "PASSU 정보 조회 API", description = """
-            PASSU 정보 조회 API 입니다. Authorization 헤더로 토큰 보내면 됩니다.
+            PASSU 정보 조회 API입니다. Authorization 헤더로 토큰 보내면 됩니다.
             """)
     @GetMapping("/user-info")
-    public ApiResponse<UserInfoResponse> getUserInfo(TokenRequest tokenRequest){
-        log.info("##### userId ##### : " + tokenRequest.getAccessToken());
-        return ApiResponse.onSuccess(userService.getUserInfo(tokenRequest.getAccessToken()));
+    public ApiResponse<UserInfoResponse> getUserInfo(
+            @RequestHeader("Authorization") String authorizationHeader){
+        String accessToken = authorizationHeader.replace("Bearer ", ""); // Bearer 제거
+        log.info("##### userId ##### : " + accessToken);
+        return ApiResponse.onSuccess(userService.getUserInfo(accessToken));
     }
 }
