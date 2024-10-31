429fd6dc590f61462b113ff3aa13abb3b4380701
jeongu <ms235280@naver.com>
2024-10-31 22:21:08 +0900

    [fix] 우선 간편하게 하나만 찾아서 보내주는 것으로 수정

--- src/main/java/ussum/homepage/application/user/service/UserService.java
+++ src/main/java/ussum/homepage/application/user/service/UserService.java
@@ -1,9 +1,10 @@
 package ussum.homepage.application.user.service;
 
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ussum.homepage.application.user.service.dto.request.TokenRequest;
 import ussum.homepage.application.user.service.dto.response.UserInfoResponse;
 import ussum.homepage.domain.csv_user.StudentCsv;
 import ussum.homepage.domain.csv_user.service.StudentCsvReader;
@@ -12,6 +13,7 @@ import ussum.homepage.domain.member.service.MemberReader;
 import ussum.homepage.domain.user.User;
 import ussum.homepage.domain.user.service.UserReader;
 import ussum.homepage.global.jwt.JwtTokenProvider;
+
 
 @Service
 @RequiredArgsConstructor
@@ -22,11 +24,26 @@ public class UserService {
     private final JwtTokenProvider provider;
     private final UserReader userReader;
 
-    public UserInfoResponse getUserInfo(String accessToken){
+    /*
+        * @param accessToken
+        * 메소드 수정 필요
+     */
+    public UserInfoResponse getUserInfo(String accessToken) {
         Long userId = provider.getSubject(accessToken);
         User user = userReader.getUserWithId(userId);
-        StudentCsv studentCsv = studentCsvReader.getStudentWithStudentId(Long.valueOf(user.getStudentId())).get();
-        Member member = memberReader.getMemberWithUserId(userId);
-        return UserInfoResponse.of(studentCsv, member);
+
+        if (user.getStudentId() != null) {
+            Optional<StudentCsv> optionalStudentCsv = studentCsvReader.getStudentWithStudentId(Long.valueOf(user.getStudentId()));
+            if (optionalStudentCsv.isPresent()) {
+                StudentCsv studentCsv = optionalStudentCsv.get();
+                List<Member> members = memberReader.getMembersWithUserId(userId);
+                Member member = members.isEmpty() ? null : members.get(0);
+                return UserInfoResponse.of(studentCsv, member);
+            }
+        }
+
+        List<Member> members = memberReader.getMembersWithUserId(userId);
+        Member member = members.isEmpty() ? null : members.get(0);
+        return UserInfoResponse.of(user, member);
     }
 }
