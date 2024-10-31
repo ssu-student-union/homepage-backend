b97dea04e0644b8e02660f540d5fc66d463b809f
jeongu <ms235280@naver.com>
2024-10-31 22:20:44 +0900

    [add] UserInfoResponse of 메소드 추가

--- src/main/java/ussum/homepage/application/user/service/dto/response/UserInfoResponse.java
+++ src/main/java/ussum/homepage/application/user/service/dto/response/UserInfoResponse.java
@@ -3,6 +3,8 @@ package ussum.homepage.application.user.service.dto.response;
 import lombok.Builder;
 import ussum.homepage.domain.csv_user.StudentCsv;
 import ussum.homepage.domain.member.Member;
+import ussum.homepage.domain.user.User;
+import ussum.homepage.infra.jpa.member.entity.MemberCode;
 
 @Builder
 public record UserInfoResponse(
@@ -16,6 +18,15 @@ public record UserInfoResponse(
                 .name(studentCsv.getStudentName())
                 .studentId(String.valueOf(studentCsv.getStudentId()))
                 .major(studentCsv.getMajor())
+                .isCouncil(member.getIsAdmin())
+                .build();
+    }
+
+    public static UserInfoResponse of(User user, Member member){
+        return UserInfoResponse.builder()
+                .name(user.getName())
+                .studentId("null")
+                .major(MemberCode.getEnumMemberCodeFromStringMemberCode(member.getMemberCode()).getStringMemberCode())
                 .isCouncil(member.getIsAdmin())
                 .build();
     }
