df9da98953ff58c9414bc211598c717a692eac82
JangInho <wkddlsgh50@naver.com>
2024-11-16 15:26:20 +0900

    [feat] #142 SuggestionPostDetailResponse 에 officialCommentList 추가

--- src/main/java/ussum/homepage/application/post/service/PostManageService.java
+++ src/main/java/ussum/homepage/application/post/service/PostManageService.java
@@ -91,7 +91,7 @@ public class PostManageService {
             "제휴게시판", (post, isAuthor, ignored, user, another_ignored1, categoryName, fileResponseList, another_ignored2, another_ignored3) -> PartnerPostDetailResponse.of(post, isAuthor, user, categoryName, fileResponseList),
             "감사기구게시판", (post, isAuthor, ignored, user, another_ignored1, categoryName, fileResponseList, another_ignored2, another_ignored3) -> AuditPostDetailResponse.of(post, isAuthor, user, categoryName, fileResponseList),
             "청원게시판", (post, isAuthor, isLiked, user, likeCount, categoryName, fileResponseList, postOfficialCommentResponseList, another_ignored3) -> PetitionPostDetailResponse.of(post, isAuthor, isLiked, user, likeCount, categoryName, fileResponseList, postOfficialCommentResponseList),
-            "건의게시판", (post, isAuthor, ignored, user, another_ignored1, categoryName, fileResponseList, another_ignored2, another_ignored3) -> SuggestionPostDetailResponse.of(post, isAuthor, user, categoryName, fileResponseList),
+            "건의게시판", (post, isAuthor, ignored, user, another_ignored1, categoryName, fileResponseList, postOfficialCommentResponseList, another_ignored3) -> SuggestionPostDetailResponse.of(post, isAuthor, user, categoryName, fileResponseList, postOfficialCommentResponseList),
             "인권신고게시판", (post, isAuthor, ignored, user, another_ignored1,categoryName, fileResponseList,postOfficialCommentResponseList,rightsDetailList) -> RightsPostDetailResponse.of(post,isAuthor,user,categoryName,fileResponseList,postOfficialCommentResponseList, rightsDetailList)
     );
 
@@ -172,13 +172,22 @@ public class PostManageService {
             Boolean isLiked = (userId != null && postReactionManager.validatePostReactionByPostIdAndUserId(postId, userId, "like"));
             response = responseFunction.apply(post, isAuthor, isLiked, user, likeCount, post.getCategory(), fileResponseList, postOfficialCommentResponses,null);
         }else if (board.getName().equals("인권신고게시판")){
+
             List<PostComment> officialPostComments = postCommentReader.getCommentListWithPostIdAndCommentType(userId, postId, "OFFICIAL");
             List<PostOfficialCommentResponse> postOfficialCommentResponses = officialPostComments.stream()
                     .map(postOfficialComment -> postOfficialCommentFormatter.format(postOfficialComment, userId))
                     .toList();
             List<RightsDetail> rightsPostDetailResponseList = postAdditionalReader.getRightsDetailByPostId(post.getId());
             response = responseFunction.apply(post, isAuthor,null,user,null,post.getCategory(),fileResponseList,postOfficialCommentResponses,rightsPostDetailResponseList);
-        } else if (board.getName().equals("제휴게시판") || board.getName().equals("공지사항게시판") || board.getName().equals("감사기구게시판") || board.getName().equals("건의게시판")) {
+
+        } else if (board.getName().equals("건의게시판")){
+            List<PostComment> officialPostComments = postCommentReader.getCommentListWithPostIdAndCommentType(userId, postId, "OFFICIAL");
+            List<PostOfficialCommentResponse> postOfficialCommentResponses = officialPostComments.stream()
+                    .map(postOfficialComment -> postOfficialCommentFormatter.format(postOfficialComment, userId))
+                    .toList();
+            response = responseFunction.apply(post, isAuthor, null, user, null, post.getCategory(), fileResponseList, postOfficialCommentResponses,null);
+
+        } else if (board.getName().equals("제휴게시판") || board.getName().equals("공지사항게시판") || board.getName().equals("감사기구게시판")) {
             response = responseFunction.apply(post, isAuthor, null, user, null, post.getCategory(), fileResponseList, null,null);
         } else if (board.getName().equals("분실물게시판")) {
             response = responseFunction.apply(post, isAuthor, null, user, null, post.getCategory(), fileResponseList, null,null); //분실물 게시판은 파일첨부 제외
--- src/main/java/ussum/homepage/application/post/service/dto/response/postDetail/SuggestionPostDetailResponse.java
+++ src/main/java/ussum/homepage/application/post/service/dto/response/postDetail/SuggestionPostDetailResponse.java
@@ -1,26 +1,32 @@
 package ussum.homepage.application.post.service.dto.response.postDetail;
 
 import java.util.List;
+
 import lombok.Builder;
+import lombok.Getter;
 import ussum.homepage.application.comment.service.dto.response.PostOfficialCommentResponse;
 import ussum.homepage.application.post.service.dto.response.FileResponse;
 import ussum.homepage.domain.post.Post;
 import ussum.homepage.domain.user.User;
 
-public class SuggestionPostDetailResponse extends PostDetailResDto{
+@Getter
+public class SuggestionPostDetailResponse extends PostDetailResDto {
     private List<FileResponse> fileResponseList;
+    private List<PostOfficialCommentResponse> officialCommentList;
     private String studentId;
 
     @Builder
     public SuggestionPostDetailResponse(Long postId, String categoryName, String authorName, String studentId, String title, String content, String createdAt, String lastEditedAt, Boolean isAuthor,
                                         List<FileResponse> fileResponseList, List<PostOfficialCommentResponse> officialCommentList,
-                                        List<String> canAuthority){
-        super(postId,categoryName,authorName,title,content,createdAt,lastEditedAt,isAuthor,canAuthority);
+                                        List<String> canAuthority) {
+        super(postId, categoryName, authorName, title, content, createdAt, lastEditedAt, isAuthor, canAuthority);
         this.fileResponseList = fileResponseList;
+        this.officialCommentList = officialCommentList;
         this.studentId = studentId;
     }
 
-    public static SuggestionPostDetailResponse of(Post post, Boolean isAuthor, User user, String categoryName, List<FileResponse> fileResponseList) {
+    public static SuggestionPostDetailResponse of(Post post, Boolean isAuthor, User user, String categoryName, List<FileResponse> fileResponseList,
+                                                  List<PostOfficialCommentResponse> officialCommentList) {
         return SuggestionPostDetailResponse.builder()
                 .postId(post.getId())
                 .authorName(user.getName())
@@ -31,6 +37,7 @@ public class SuggestionPostDetailResponse extends PostDetailResDto{
                 .lastEditedAt(post.getLastEditedAt())
                 .isAuthor(isAuthor)
                 .fileResponseList(fileResponseList)
+                .officialCommentList(officialCommentList)
                 .build();
     }
 }
