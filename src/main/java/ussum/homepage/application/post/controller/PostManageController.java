400bc5da6790417ca35594b528f6ec58bb415bda
JangInho <wkddlsgh50@naver.com>
2025-02-16 18:15:46 +0900

    [merge] 운영 서버 배포 conflict 수정 (#212)

--- src/main/java/ussum/homepage/application/post/controller/PostManageController.java
+++ src/main/java/ussum/homepage/application/post/controller/PostManageController.java
@@ -27,12 +27,13 @@ import ussum.homepage.global.config.custom.BoardRequestBody;
 public class PostManageController {
 
     private final PostManageService postManageService;
+
     @Operation(summary = "게시판 별 게시물 리스트 조회 api", description = """
             게시판 별 게시물 리스트 조회 시 필요한 데이터를 조회하는 api 입니다.
             요청으로 boardCode 그리고 queryParam 형식으로 , groupCode(중앙기구, 단과대학생회), memberCode(중앙운영위원회), category(필터링),page(입력 안 할시 첫번째 페이지), take(몇개 가져올지) 값을 넣으면 됩니다.
             공지사항게시판을 사용할때만 groupCode, memberCode에 값을 넣어서 사용하시면 됩니다. 
             나머지 게시판 필터링은 category에 값을 넣고 사용하시면 됩니다.
-            
+                        
             인권신고게시판의 경우 조회 권한과 글쓰기 권한이 allowedAuthorities, deniedAuthorities에 담겨집니다. 각각 허용하는 권한과 허용하지 않는 권한이 담깁니다.
             학생인권위원회가 조회할시 allowedAuthorities에 ALL_READ가 담겨집니다. 이외의 자치기구/비로그인 deniedAuthorities에 ALL_READ와 WRITE권한이 들어갑니다.
             로그인의 경우 allowedAuthorities의 WRITE만 담기지만 만약 게시물 리스트의 author 필드가 true라면 게시물의 작성자로 리스트중 해당 게시물만 조회가능합니다. 
@@ -41,9 +42,9 @@ public class PostManageController {
     public ResponseEntity<ApiResponse<?>> getBoardPostsList(@Parameter(hidden = true) @UserId Long userId,
                                                             @RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "take") int take,
                                                             @PathVariable(name = "boardCode") String boardCode, @RequestParam(value = "groupCode", required = false) String groupCode,
-                                                            @RequestParam(value = "memberCode",  required = false) String memberCode,
-                                                            @RequestParam(value = "category",  required = false) String category,
-                                                            @RequestParam(value = "suggestionTarget",  required = false) String suggestionTarget,
+                                                            @RequestParam(value = "memberCode", required = false) String memberCode,
+                                                            @RequestParam(value = "category", required = false) String category,
+                                                            @RequestParam(value = "suggestionTarget", required = false) String suggestionTarget,
                                                             @RequestParam(value = "qnaMajorCode", required = false) String qnaMajorCode,
                                                             @RequestParam(value = "qnaMemberCode", required = false) String qnaMemberCode
     ) {
@@ -59,7 +60,7 @@ public class PostManageController {
     @GetMapping("data/posts")
     public ResponseEntity<ApiResponse<?>> getDataPostsList(@Parameter(hidden = true) @UserId Long userId,
                                                            @RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "take") int take,
-                                                           @RequestParam(name = "majorCategory", required = false) String majorCategory, @RequestParam(name = "middleCategory", required = false) String middleCategory,@RequestParam(name = "subCategory", required = false) String subCategory) {
+                                                           @RequestParam(name = "majorCategory", required = false) String majorCategory, @RequestParam(name = "middleCategory", required = false) String middleCategory, @RequestParam(name = "subCategory", required = false) String subCategory) {
 
         return ApiResponse.success(postManageService.getDataList(userId, page, take, majorCategory, middleCategory, subCategory));
     }
@@ -71,7 +72,7 @@ public class PostManageController {
             이때 로그인 유저가 조회 시 토큰 발급 시 같이 반환 되었던 userId 값을 이용하여, 해당 user가 해당 댓글 혹은 대댓글을 작성하였는지의 여부를 판단하기 위하여 isAuthor 필드를 이용하였습니다.
             userId를 넣고 반환되는 isAuthor 필드가 true 라면 해당 user는 본인이 작성한 게시물 임을 나타냅니다. 
             하지만 isAuthor 필드가 false 라면(비로그인 포함) 해당 user는 본인이 작성한 게시물이 아니기에 수정, 삭제를 막아야 합니다. 
-            
+                        
             + boardCode가 청원게시판일 때는 중앙운영위원회가 해당 게시물에 댓글을 작성했을 시 postOfficialCommentResponses라는 List 필드 값에 중앙운영위원회가 작성한 공식답변이 담기게 됩니다.
             + 인권신고게시판일때 학생인권위원회가 해당 게시물에 댓글 달았을때에도 "officialCommentList"에 공식답변이 담기게 됩니다.  
             """)
@@ -88,20 +89,20 @@ public class PostManageController {
             PathVariable로 노션에 있는 boardCode중 하나를 적습니다.
             만약 boardCode가 "공지사항게시판" 이라면 PostCreateRequest dto의 category필드에 값을 넣지 않습니다.
             그 이유는 공지사항게시판의 카테고리는 글을 쓸 수 있는 작성자의 groupCode와 memberCode에 따라 필터링되어 조회되기 때문입니다.
-            
+                        
             JSON 형식으로 PostCreateRequest dto를 활용하여 게시글에 적은 값을 전달하면 됩니다.
             PostCreateRequest dto의 List로 되어있는 변수에 들어가는 값 s3에 저장하고 나온 url입니다. 
             이 컨트롤러에 있는 "/board/{boardCode}/files" api를 먼저 사용하여 리턴값으로 전달받는 값을 넣어주면 됩니다. 
             사진이나 파일이 존재하지 않을 시 빈 List로 전달해주시면 됩니다.
             isNotice는 긴급공지 사항을 나타내는 필드로 맞을시 true, 틀릴시 false를 반환하면 됩니다.
-            
+                        
             인권신고게시판 경우 기존 json에 추가로 relatedPeople 리스트로 전달해주시면 됩니다. 리스트에는 name, major, studentId에 값을 String으로
             넣어서 전달해주시고 personType은 신고자, 피침해자, 침해자중 선택하여 전달해주시면 됩니다.
             """)
     @PostMapping("/{boardCode}/posts")
     public ResponseEntity<ApiResponse<?>> createBoardPost(@Parameter(hidden = true) @UserId Long userId,
                                                           @PathVariable(name = "boardCode") String boardCode,
-                                                          @BoardRequestBody PostCreateRequest postCreateRequest){
+                                                          @BoardRequestBody PostCreateRequest postCreateRequest) {
         return ApiResponse.success(postManageService.createBoardPost(userId, boardCode, postCreateRequest));
     }
 
@@ -116,6 +117,7 @@ public class PostManageController {
                                                          @RequestBody GeneralPostCreateRequest generalPostCreateRequest) {
         return ApiResponse.success(postManageService.createDataPost(userId, fileCategory, generalPostCreateRequest));
     }
+
     @Operation(summary = "게시물 생성 시 파일 및 이미지 저장 api", description = """
             게시물 생성 시 파일 및 이미지 저장하는 api입니다.
             우선 이 api는 게시물 생성 api를 사용전 미리 호출하여야 합니다. 기본적으로 반환값이 저장된 파일 및 사진의 url이기 때문에
@@ -143,8 +145,8 @@ public class PostManageController {
     @PostMapping(value = "/data/files/{fileType}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiResponse<?>> createBoardDataPostFile(@Parameter(hidden = true) @UserId Long userId,
-                                                              @RequestPart(value = "files", required = false) MultipartFile[] files,
-                                                              @PathVariable(name = "fileType") String fileType) {
+                                                                  @RequestPart(value = "files", required = false) MultipartFile[] files,
+                                                                  @PathVariable(name = "fileType") String fileType) {
         return ApiResponse.success(postManageService.createBoardDataPostFile(userId, files, fileType));
     }
 
@@ -155,7 +157,7 @@ public class PostManageController {
     @DeleteMapping("/{boardCode}/files")
     public ResponseEntity<ApiResponse<?>> deleteBoardPostFile(@Parameter(hidden = true) @UserId Long userId,
                                                               @PathVariable(name = "boardCode") String boardCode,
-                                                              @RequestBody PostFileDeleteRequest postFileDeleteRequest){
+                                                              @RequestBody PostFileDeleteRequest postFileDeleteRequest) {
         return ApiResponse.success(postManageService.deleteBoardPostFile(userId, boardCode, postFileDeleteRequest));
     }
 
@@ -170,14 +172,15 @@ public class PostManageController {
                                                         @RequestBody PostUpdateRequest postUpdateRequest) {
         return ApiResponse.success(postManageService.editBoardPost(boardCode, postId, postUpdateRequest));
     }
+
     @Operation(summary = "자료집 게시물 수정 api", description = """
             자료집 게시물을 수정하는 api 입니다. 
             """)
     @PatchMapping("/data/{fileCategory}/posts/{postId}")
     public ResponseEntity<ApiResponse<?>> editBoardDataPost(@Parameter(hidden = true) @UserId Long userId,
-                                                        @PathVariable(name = "fileCategory") String fileCategory,
+                                                            @PathVariable(name = "fileCategory") String fileCategory,
                                                             @PathVariable(name = "postId") Long postId,
-                                                        @RequestBody PostUpdateRequest postUpdateRequest) {
+                                                            @RequestBody PostUpdateRequest postUpdateRequest) {
         return ApiResponse.success(postManageService.editBoardDatePost(fileCategory, postId, postUpdateRequest));
     }
 
@@ -207,11 +210,11 @@ public class PostManageController {
     public ResponseEntity<ApiResponse<?>> searchBoardPost(@Parameter(hidden = true) @UserId Long userId,
                                                           @RequestParam(value = "page", defaultValue = "0") int page,
                                                           @RequestParam(value = "take") int take,
-                                                          @RequestParam(value = "q",required = false) String q,
+                                                          @RequestParam(value = "q", required = false) String q,
                                                           @PathVariable(name = "boardCode") String boardCode,
                                                           @RequestParam(value = "groupCode", required = false) String groupCode,
-                                                          @RequestParam(value = "memberCode",  required = false) String memberCode,
-                                                          @RequestParam(value = "category",  required = false) String category,
+                                                          @RequestParam(value = "memberCode", required = false) String memberCode,
+                                                          @RequestParam(value = "category", required = false) String category,
                                                           @RequestParam(value = "qnaMajorCode", required = false) String qnaMajorCode,
                                                           @RequestParam(value = "qnaMemberCode", required = false) String qnaMemberCode
     ) {
@@ -228,7 +231,7 @@ public class PostManageController {
     public ResponseEntity<ApiResponse<?>> searchDataPostsList(@Parameter(hidden = true) @UserId Long userId,
                                                               @RequestParam(value = "page", defaultValue = "0") int page,
                                                               @RequestParam(value = "take") int take,
-                                                              @RequestParam(value = "q",required = false) String q,
+                                                              @RequestParam(value = "q", required = false) String q,
                                                               @RequestParam(name = "majorCategory", required = false) String majorCategory,
                                                               @RequestParam(name = "middleCategory", required = false) String middleCategory,
                                                               @RequestParam(name = "subCategory", required = false) String subCategory) {
@@ -255,7 +258,7 @@ public class PostManageController {
             이때 로그인 유저가 조회 시 토큰 발급 시 같이 반환 되었던 userId 값을 이용하여, 해당 user가 해당 댓글 혹은 대댓글을 작성하였는지의 여부를 판단하기 위하여 isAuthor 필드를 이용하였습니다.
             userId를 넣고 반환되는 isAuthor 필드가 true 라면 해당 user는 본인이 작성한 게시물 임을 나타냅니다. 
             하지만 isAuthor 필드가 false 라면(비로그인 포함) 해당 user는 본인이 작성한 게시물이 아니기에 수정, 삭제를 막아야 합니다. 
-            
+                        
             + boardCode가 청원게시판일 때는 중앙운영위원회가 해당 게시물에 댓글을 작성했을 시 postOfficialCommentResponses라는 List 필드 값에 중앙운영위원회가 작성한 공식답변이 담기게 됩니다.
             + 인권신고게시판일때 학생인권위원회가 해당 게시물에 댓글 달았을때에도 "officialCommentList"에 공식답변이 담기게 됩니다.  
             """)
@@ -281,9 +284,9 @@ public class PostManageController {
             """)
     @GetMapping("/mypost/search")
     public ResponseEntity<ApiResponse<?>> searchMyPost(@Parameter(hidden = true) @UserId Long userId,
-                                                          @RequestParam(value = "page", defaultValue = "0") int page,
-                                                          @RequestParam(value = "take") int take,
-                                                          @RequestParam(value = "q",required = false) String q
+                                                       @RequestParam(value = "page", defaultValue = "0") int page,
+                                                       @RequestParam(value = "take") int take,
+                                                       @RequestParam(value = "q", required = false) String q
     ) {
         return ApiResponse.success(postManageService.searchMyPost(userId, page, take, q));
     }
@@ -297,12 +300,4 @@ public class PostManageController {
         return ApiResponse.onSuccess(response);
     }
 
-    @Operation(summary = "단과대 학과 조회 api", description = """
-            단과대 학과 리스트 조회하는 api입니다.
-            """)
-    @GetMapping("/colleges-departments")
-    public ApiResponse<CollegeAndDepartmentResponse> getCollegeAndDepartmentList(@Parameter(hidden = true) @UserId Long userId) {
-        CollegeAndDepartmentResponse response = postManageService.getCollegeAndDepartment(userId);
-        return ApiResponse.onSuccess(response);
-    }
 }
