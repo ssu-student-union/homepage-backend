c5d8717f5bef9f6c50cb62b98fa2f92b7fcd4178
최인준 <dlswns661035@gmail.com>
2025-03-14 21:53:32 +0900

    [feat] RedisConfig

--- src/main/java/ussum/homepage/infra/config/RedisConfig.java
+++ src/main/java/ussum/homepage/infra/config/RedisConfig.java
@@ -1,47 +1,57 @@
-//package ussum.homepage.infra.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-//import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-//@RequiredArgsConstructor
-//@Configuration
-//@EnableRedisRepositories
-//public class RedisConfig {
-//    @Value("${spring.data.redis.host}")
-//    private String host;
-//
-//    @Value("${spring.data.redis.port}")
-//    private int port;
-//
-//    @Value("${spring.data.redis.password}")
-//    private String password;
-//
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory() { //lettuce
-//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-//        redisStandaloneConfiguration.setHostName(host);
-//        redisStandaloneConfiguration.setPort(port);
-//        redisStandaloneConfiguration.setPassword(password);
-//        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder().build();
-//
-//        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() { //redis-cli 사용을 위한 설정
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(redisConnectionFactory());
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new StringRedisSerializer());
-//        return redisTemplate;
-//    }
-//}
+package ussum.homepage.infra.config;
+
+import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisClient redisClient() {
+        return RedisClient.create(RedisURI.builder()
+            .withHost(host)
+            .withPort(port)
+            .build());
+    }
+
+    @Bean
+    public StatefulRedisConnection<String, byte[]> redisConnection(RedisClient redisClient) {
+        return redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+    }
+
+    @Bean
+    public ProxyManager<String> lettuceBasedProxyManager() {
+        RedisClient redisClient = redisClient();
+        StatefulRedisConnection<String, byte[]> redisConnection = redisClient
+            .connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+
+        // Expiration 전략 설정
+        return LettuceBasedProxyManager.builderFor(redisConnection)
+            .withExpirationStrategy(
+                ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(
+                    Duration.ofMinutes(1L)))
+            .build();
+    }
+
+}
