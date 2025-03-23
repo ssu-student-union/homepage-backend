cc58757bacca1e15ff07a7d2d24edc751e947127
배현서 <haulqogustj@naver.com>
2025-03-23 18:14:29 +0900

    fix: post_file 테이블의 file_category 컬럼 크기 수정
    file_category 컬럼에 데이터 잘림 오류(Data truncated) 해결을 위해
    컬럼 길이를 확장하여 FileCategory enum 값을 온전히 저장할 수 있도록 수정

--- src/main/java/ussum/homepage/infra/jpa/post/entity/PostFileEntity.java
+++ src/main/java/ussum/homepage/infra/jpa/post/entity/PostFileEntity.java
@@ -16,6 +16,7 @@ public class PostFileEntity {
     private String typeName;
 
     @Enumerated(EnumType.STRING)
+    @Column(length = 100) // 적절한 길이로 설정
     private FileCategory fileCategory;
     private String url;
     private String size;
