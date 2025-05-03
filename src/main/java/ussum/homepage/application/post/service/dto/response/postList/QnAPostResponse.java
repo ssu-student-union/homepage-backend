117f685c6c5b431d21e8acce74ba00b679c9fe15
Inho Jang <71584696+JangInho@users.noreply.github.com>
2025-05-03 19:50:42 +0900

    [feat] : #285 질의응답게시판 이름 마스킹 (#289)

--- src/main/java/ussum/homepage/application/post/service/dto/response/postList/QnAPostResponse.java
+++ src/main/java/ussum/homepage/application/post/service/dto/response/postList/QnAPostResponse.java
@@ -13,15 +13,18 @@ public class QnAPostResponse extends PostListResDto {
 
     String department;
     String college;
+    String authorName; // 마스킹된 이름을 응답함 단, 자치기구 계정은 제외 ex) 장*호, 총학생회
 
     @Builder
-    protected QnAPostResponse(Long postId, String title, String content, String date, String category, String department, String college) {
+    protected QnAPostResponse(Long postId, String title, String content, String date, String category, String department, String college, String authorName) {
         super(postId, title, content, date, category);
         this.department = department;
         this.college = college;
+        this.authorName = authorName;
     }
 
-    public static QnAPostResponse of(Post post, Member member) {
+    public static QnAPostResponse of(Post post, Member member, User user) {
+
         return QnAPostResponse.builder()
                 .postId(post.getId())
                 .title(post.getTitle())
@@ -30,6 +33,7 @@ public class QnAPostResponse extends PostListResDto {
                 .category(post.getCategory())
                 .department(MajorCode.getEnumMajorCodeFromStringMajorCode(member.getMajorCode()).getStringMajorCode())
                 .college(MemberCode.getEnumMemberCodeFromStringMemberCode(member.getMemberCode()).getStringMemberCode())
+                .authorName(member.getIsAdmin() ? user.getName() : User.maskedName(user.getName()))
                 .build();
     }
 }
